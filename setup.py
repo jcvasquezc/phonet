9cd59f31848db79a4765655e489713edc998d065
jcvasquezc <juancamilo0628@gmail.com>
2019-03-09 11:15:03 -0500

    tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 install_requires = [
-    'tensorflow'
+    'tensorflow',
     'Keras',
     'pandas',
     'python_speech_features'
