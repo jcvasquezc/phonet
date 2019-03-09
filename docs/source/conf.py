7cdb8d92e28f1973a15f2e84584df1af15a5c0b1
jcvasquezc <juancamilo0628@gmail.com>
2019-03-09 11:07:33 -0500

    tests

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
    def __getattr__(cls, name):
        return MagicMock()
 
-MOCK_MODULES = ['tensorflow', 'tensorflow.python']
+MOCK_MODULES = ['tensorflow', 'tensorflow.python', 'tensorflow.python.framework']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
